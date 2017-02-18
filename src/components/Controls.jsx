6c10b16d6f423ad191ee4308acd15b31477f240c
Erik <erik.secker@cognitivewave.com>
2017-02-18 17:23:10 -0500

    Move sprite through mouse and keyboard events (with Egan where he did some of the programming; same for previous three commits)

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import keydown from 'react-keydown';
 
 import { INTERVAL_RATE } from '../constants';
+
+const ARROW_CODE_LEFT = 37;
+const ARROW_CODE_UP = 38;
+const ARROW_CODE_DOWN = 40;
+const ARROW_CODE_RIGHT = 39;
+
 
 import * as actions from '../reducers/hero';
 
@@ -13,24 +20,94 @@ export class Controls extends Component {
 		};
 	}
 
+	componentWillReceiveProps( { keydown } ) {
+		const { hero, updateModel } = this.props;
+
+    if ( keydown.event ) {
+      // inspect the keydown event and decide what to do
+			switch( keydown.event.which ) {
+				case ARROW_CODE_LEFT:
+					hero.moveLeft();
+					updateModel( hero );
+					break;
+				case ARROW_CODE_UP:
+					hero.moveUp();
+					updateModel( hero );
+					break;
+				case ARROW_CODE_DOWN:
+					hero.moveDown();
+					updateModel( hero );
+					break;
+				case ARROW_CODE_RIGHT:
+					hero.moveRight();
+					updateModel( hero );
+					break;
+				default:
+					break;
+			}
+
+    }
+  }
+
 	moveUp() {
+		const { hero, updateModel } = this.props;
+
+		clearInterval( this.state.intervalId );
+		const intervalId = setInterval( () => {
+			hero.moveUp();
+			updateModel(hero);
+		}, INTERVAL_RATE);
+		this.setState({ intervalId });
 	}
 
 	moveDown() {
+		const { hero, updateModel } = this.props;
+
+		clearInterval( this.state.intervalId );
+		const intervalId = setInterval( () => {
+			hero.moveDown();
+			updateModel(hero);
+		}, INTERVAL_RATE);
+		this.setState({ intervalId });
 	}
 
 	moveLeft() {
+		const { hero, updateModel } = this.props;
+
+		clearInterval( this.state.intervalId );
+		const intervalId = setInterval( () => {
+			hero.moveLeft();
+			updateModel(hero);
+		}, INTERVAL_RATE);
+		this.setState({ intervalId });
 	}
 
 	moveRight() {
+		const { hero, updateModel } = this.props;
+
+		clearInterval( this.state.intervalId );
+		const intervalId = setInterval( () => {
+			hero.moveRight();
+			updateModel(hero);
+		}, INTERVAL_RATE);
+		this.setState({ intervalId });
 	}
 
 	stopMove() {
+		clearInterval( this.state.intervalId );
 	}
 
 	render() {
 		return (
 			<div id="controls">
+				<button
+					type="button"
+					onMouseDown={ this.moveLeft.bind(this) }
+					onMouseUp={ this.stopMove.bind(this) }
+				>
+					<span className="fa fa-caret-left" />
+				</button>
+
 				<button
 					type="button"
 					onMouseDown={ this.moveUp.bind(this) }
@@ -46,6 +123,15 @@ export class Controls extends Component {
 				>
 					<span className="fa fa-caret-down" />
 				</button>
+				<button
+					type="button"
+					onMouseDown={ this.moveRight.bind(this) }
+					onMouseUp={ this.stopMove.bind(this) }
+				>
+					<span className="fa fa-caret-right" />
+				</button>
+
+
 			</div>
 		);
 	}
@@ -56,5 +142,5 @@ function mapStateToProps(state) {
 		hero: state.hero.model,
 	}
 }
-
+Controls = keydown(Controls);
 export default connect(mapStateToProps, actions)(Controls);
