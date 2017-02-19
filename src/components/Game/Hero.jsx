3d2fd7bca877d1c5e4ecdebda12d5ec44f3e7691
Erik <erik.secker@cognitivewave.com>
2017-02-19 17:18:11 -0500

    Initiializing hero object properties for rendering the composite body and animation ranges

--- src/components/Game/Hero.jsx
+++ src/components/Game/Hero.jsx
@@ -13,6 +13,7 @@ const HERO_BODY_WIDTH = 583;
 const HERO_BODY_HEIGHT = 492;
 const HERO_BODY_X = 136;
 const HERO_BODY_Y = 0;
+
 const HERO_LEFT_LEG_WIDTH = 197;
 const HERO_LEFT_LEG_HEIGHT = 369;
 const HERO_LEFT_LEG_DOWN_X = 0;
@@ -25,7 +26,7 @@ const HERO_RIGHT_LEG_DOWN_X = 427;
 const HERO_RIGHT_LEG_DOWN_Y = 212;
 const HERO_RIGHT_LEG_UP_X = 411;
 const HERO_RIGHT_LEG_UP_Y = 193;
-
+const HERO_LEG_SPEED = 10;
 
 import * as actions from '../../reducers/hero';
 
@@ -33,38 +34,61 @@ export class Hero extends Component {
 	componentWillMount() {
 		const { hero, updateModel } = this.props;
 
-		hero.setPosition( HERO_INIT_X, HERO_INIT_Y )
+		// INIT HERO WHOLE
 		hero.setWidth(HERO_WIDTH);
 		hero.setHeight(HERO_HEIGHT);
+		hero.setPosition( HERO_INIT_X, HERO_INIT_Y )
 		hero.setSpeed(HERO_SPEED);
+
+		// INIT HERO BODY
+		hero.setWidth(HERO_BODY_WIDTH);
+		hero.setHeight(HERO_BODY_HEIGHT);
+		hero.body.setPosition( HERO_BODY_X, HERO_BODY_Y );
+
+		// INIT HERO LEFT LEG AND ANIMATION RANGES
+		hero.leftLeg.setWidth( HERO_LEFT_LEG_WIDTH );
+		hero.leftLeg.setHeight( HERO_LEFT_LEG_HEIGHT );
+		hero.leftLeg.setPosition( HERO_LEFT_LEG_DOWN_X, HERO_LEFT_LEG_DOWN_Y );
+		hero.leftLeg.setSpeed( HERO_LEG_SPEED );
+		hero.leftLeg.setAnimateRangeStartPos( HERO_LEFT_LEG_DOWN_X, HERO_LEFT_LEG_DOWN_Y );
+		hero.leftLeg.setAnimateRangeEndPos( HERO_LEFT_LEG_UP_X, HERO_LEFT_LEG_UP_Y );
+
+		// INIT HERO RIGHT LEG AND ANIMATION RANGES
+		hero.rightLeg.setWidth( HERO_RIGHT_LEG_WIDTH );
+		hero.rightLeg.setHeight( HERO_RIGHT_LEG_HEIGHT );
+		hero.rightLeg.setPosition( HERO_RIGHT_LEG_UP_X, HERO_RIGHT_LEG_UP_Y );
+		hero.rightLeg.setSpeed( HERO_LEG_SPEED );
+		hero.rightLeg.setAnimateRangeStartPos( HERO_RIGHT_LEG_UP_X, HERO_RIGHT_LEG_UP_Y );
+		hero.rightLeg.setAnimateRangeEndPos( HERO_RIGHT_LEG_DOWN_X, HERO_RIGHT_LEG_DOWN_Y );
+
 		updateModel(hero);
 
 	}
 
 	render() {
 		const { hero } = this.props;
-		// console.log( "x:", hero.xPos, ", y:", hero.yPos );
+		console.log( hero );
 
 		const heroStyles = {
 			left: `${hero.xPos}px`,
 			top: `${hero.yPos}px`,
-			width: `${HERO_WIDTH}px`,
-			height: `${HERO_HEIGHT}px`,
+			width: `${hero.width}px`,
+			height: `${hero.height}px`,
 		}
 
 		const bodyStyles = {
-			left: `${HERO_BODY_X}px`,
-			top: `${HERO_BODY_Y}px`,
+			left: `${hero.body.xPos}px`,
+			top: `${hero.body.yPos}px`,
 		}
 
 		const leftLegStyles = {
-			left: `${HERO_LEFT_LEG_DOWN_X}px`,
-			top: `${HERO_LEFT_LEG_DOWN_Y}px`,
+			left: `${hero.leftLeg.xPos}px`,
+			top: `${hero.leftLeg.yPos}px`,
 		}
 
 		const rightLegStyles = {
-			left: `${HERO_RIGHT_LEG_UP_X}px`,
-			top: `${HERO_RIGHT_LEG_UP_Y}px`,
+			left: `${hero.rightLeg.xPos}px`,
+			top: `${hero.rightLeg.yPos}px`,
 		}
 
 
