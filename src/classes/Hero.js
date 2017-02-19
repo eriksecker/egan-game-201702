ceadd39fb2341b24eaeaa9ddc96aef4b0ef5c2d4
Erik <erik.secker@cognitivewave.com>
2017-02-19 17:17:07 -0500

    Adding Hero body, leftLeg, and rightLeg properties

--- src/classes/Hero.js
+++ src/classes/Hero.js
@@ -2,9 +2,20 @@ import Sprite from './Sprite';
 
 export class Hero extends Sprite {
 	constructor({
+		body,
+		leftLeg,
+		rightLeg,
 		...props,
 	}) {
 		super(props);
+
+		const bodyProps = (body) ? body : {};
+		const leftLegProps = (leftLeg) ? leftLeg : {};
+		const rightLegProps = (rightLeg) ? rightLeg : {};
+
+		this.body = new Sprite(bodyProps);
+		this.leftLeg = new Sprite(leftLegProps);
+		this.rightLeg = new Sprite(rightLegProps);
 	}
 }
 
