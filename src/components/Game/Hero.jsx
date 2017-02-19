487f8bd68244add3bb831e2d50838ab6a3208e00
Erik <erik.secker@cognitivewave.com>
2017-02-19 12:58:43 -0500

    Breaking the dragon into animatable pieces

--- src/components/Game/Hero.jsx
+++ src/components/Game/Hero.jsx
@@ -2,10 +2,30 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 const HERO_SPEED = 10;
-const HERO_WIDTH = 150;
-const HERO_HEIGHT = 113;
-const HERO_INIT_X = 200;
-const HERO_INIT_Y = 200;
+const HERO_WIDTH = 720;
+const HERO_HEIGHT = 546;
+// const HERO_INIT_X = 200;
+// const HERO_INIT_Y = 200;
+const HERO_INIT_X = 20;
+const HERO_INIT_Y = 20;
+
+const HERO_BODY_WIDTH = 583;
+const HERO_BODY_HEIGHT = 492;
+const HERO_BODY_X = 136;
+const HERO_BODY_Y = 0;
+const HERO_LEFT_LEG_WIDTH = 197;
+const HERO_LEFT_LEG_HEIGHT = 369;
+const HERO_LEFT_LEG_DOWN_X = 0;
+const HERO_LEFT_LEG_DOWN_Y = 141;
+const HERO_LEFT_LEG_UP_X = 15;
+const HERO_LEFT_LEG_UP_Y = 116;
+const HERO_RIGHT_LEG_WIDTH = 217;
+const HERO_RIGHT_LEG_HEIGHT = 332;
+const HERO_RIGHT_LEG_DOWN_X = 427;
+const HERO_RIGHT_LEG_DOWN_Y = 212;
+const HERO_RIGHT_LEG_UP_X = 411;
+const HERO_RIGHT_LEG_UP_Y = 193;
+
 
 import * as actions from '../../reducers/hero';
 
@@ -25,16 +45,43 @@ export class Hero extends Component {
 		const { hero } = this.props;
 		// console.log( "x:", hero.xPos, ", y:", hero.yPos );
 
-		const styles = {
+		const heroStyles = {
 			left: `${hero.xPos}px`,
 			top: `${hero.yPos}px`,
-			width: `${hero.width}px`,
-			height: `${hero.height}px`,
+			width: `${HERO_WIDTH}px`,
+			height: `${HERO_HEIGHT}px`,
 		}
 
+		const bodyStyles = {
+			left: `${HERO_BODY_X}px`,
+			top: `${HERO_BODY_Y}px`,
+		}
+
+		const leftLegStyles = {
+			left: `${HERO_LEFT_LEG_DOWN_X}px`,
+			top: `${HERO_LEFT_LEG_DOWN_Y}px`,
+		}
+
+		const rightLegStyles = {
+			left: `${HERO_RIGHT_LEG_UP_X}px`,
+			top: `${HERO_RIGHT_LEG_UP_Y}px`,
+		}
+
+
+
 		return (
-			<div className="hero" style={ styles }>
-				<img src="images/dragon.png" />
+			<div className="hero" style={ heroStyles }>
+				<div className="wrapper">
+					<div className="body" style={ bodyStyles }>
+						<img src="images/dragon-body.png" />
+					</div>
+					<div className="left-leg" style={ leftLegStyles }>
+						<img src="images/dragon-left-leg.png" />
+					</div>
+					<div className="right-leg" style={ rightLegStyles }>
+						<img src="images/dragon-right-leg.png" />
+					</div>
+				</div>
 			</div>
 		);
 	}
