be0b3e6b1acb4e42570bfd133c0eeee98f570b23
Erik <erik.secker@cognitivewave.com>
2017-04-01 15:10:05 -0400

    Added leg images; positioned legs; scaled all images and locations

--- src/components/Game/Hero.jsx
+++ src/components/Game/Hero.jsx
@@ -6,28 +6,28 @@ const HERO_SCALE = .3;
 const HERO_SPEED = 10;
 const HERO_WIDTH = 720;
 const HERO_HEIGHT = 546;
-const HERO_INIT_X = 10;
-const HERO_INIT_Y = 10;
+const HERO_INIT_X = 200;
+const HERO_INIT_Y = 200;
 
 const HERO_BODY_WIDTH = 583;
 const HERO_BODY_HEIGHT = 492;
-const HERO_BODY_X = 0;
+const HERO_BODY_X = 136;
 const HERO_BODY_Y = 0;
 
 const HERO_LEFT_LEG_WIDTH = 197;
 const HERO_LEFT_LEG_HEIGHT = 369;
 const HERO_LEFT_LEG_DOWN_X = 0;
-const HERO_LEFT_LEG_DOWN_Y = 0;
-const HERO_LEFT_LEG_UP_X = 0;
-const HERO_LEFT_LEG_UP_Y = 0;
+const HERO_LEFT_LEG_DOWN_Y = 141;
+const HERO_LEFT_LEG_UP_X = 13;
+const HERO_LEFT_LEG_UP_Y = 118;
 const HERO_RIGHT_LEG_WIDTH = 217;
 const HERO_RIGHT_LEG_HEIGHT = 332;
-const HERO_RIGHT_LEG_DOWN_X = 0;
-const HERO_RIGHT_LEG_DOWN_Y = 0;
-const HERO_RIGHT_LEG_UP_X = 0;
-const HERO_RIGHT_LEG_UP_Y = 0;
+const HERO_RIGHT_LEG_DOWN_X = 426;
+const HERO_RIGHT_LEG_DOWN_Y = 214;
+const HERO_RIGHT_LEG_UP_X = 411;
+const HERO_RIGHT_LEG_UP_Y = 195;
 const HERO_LEG_SPEED = 10;
-const HERO_LEG_ANIMATE_INTERVAL_RATE = 250;
+const HERO_LEG_ANIMATE_INTERVAL_RATE = 200;
 const HERO_LEG_ANIMATION_STEPS = 1;
 
 import * as actions from '../../reducers/hero';
@@ -136,6 +136,12 @@ export class Hero extends Component {
 				<div className="wrapper">
 					<div className="body" style={ bodyStyles }>
 						<img src="images/dragon-body.png" />
+					</div>
+					<div className="left-leg" style={ leftLegStyles }>
+						<img src="images/dragon-left-leg.png" />
+					</div>
+					<div className="right-leg" style={ rightLegStyles }>
+						<img src="images/dragon-right-leg.png" />
 					</div>
 				</div>
 			</div>
